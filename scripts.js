0cea16e40fa7fd6aa824d61743748f4a3da5a531
AdamNatur <adam.natur@mail.ru>
2023-02-17 23:50:09 +0300

    add points 5, 6, 7

--- scripts.js
+++ scripts.js
@@ -66,7 +66,85 @@ switch(result) {
         break
     
     case 4:
+
+        string = "";
+        for(i = 1; i <= 12; i++){
+
+            if(i % 2 === 0){
+                for(k = 1; k <= i; k++){
+                    string +="#";
+                }
+                string +="\n"; 
+            }
+            else{
+                for(k = 1; k <= i; k++){
+                    string +="*";
+                }
+                string +="\n"; 
+            }
+        }  
+        string += "||"
+        console.log(string)  
+
+        break
+
+    case 5:
         
+        n = 0
+        x = 0
+        y = 0
+
+        while(x*y*n <= 0)
+        {
+            alert("Введите целые числа")
+            n = prompt("Число")
+            x = prompt("Возможный делитель 1")
+            y = prompt("Возможный делитель 2")
+            n = +n
+            x = +x
+            y = +y
+        }
+
+        if ((n % x === 0) & (n % y === 0)){
+
+            check = true;
+            console.log("n = " + n + ", x = " + x + ", y = " + y + " = " + check)
+        }
+        else{
+            check = false;
+            console.log("n = " + n + ", x = " + x + ", y = " + y + " = " + check)
+        }
+
+        break;
+
+    case 7:
+
+        month = 0;
+
+        while(month < 1 | month > 12 | month === NaN)
+        {
+            alert("Задайте число от 1 до 12")
+            month = prompt("Номер месяца")
+            month = +month
+        }
+
+        if( month <= 3){
+            quarter = 1;
+        }
+        if( month > 3 | month <= 6){
+            quarter = 2;
+        }
+        if( month > 6 | month <= 9){
+            quarter = 3;
+        }
+        if( month > 9 | month <= 12){
+            quarter = 4;
+        }
+
+        console.log("месяц " + month + " => " + quarter + " квартал ")
+
+        break
+
     default:
         result = prompt("Номер задачи", '');
       break
